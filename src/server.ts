4cbaa248e7de51df2c989f9e55ac5be8ab9a5ce8
Eduardo Nunes <nunes_eduardo@rocketmail.com>
2020-12-05 03:48:36 +0000

    config do banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 
+import './database'
+
 import routes from './routes'
 
 const app = express()
+const port = 3333
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
-app.listen(3333, () => {
-  console.log('rodando!')
+app.listen(port, () => {
+  console.log(`Servidor executando na porta ${port}`)
 })
