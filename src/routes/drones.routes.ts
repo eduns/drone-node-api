2bedec960409ec5005d0144aebead1cfb665901b
Eduardo Nunes <nunes_eduardo@rocketmail.com>
2020-12-07 07:21:06 +0000

    inclusão do controller  nas rotas

--- src/routes/drones.routes.ts
+++ src/routes/drones.routes.ts
@@ -1,59 +1,19 @@
-import { Router, Request, Response } from 'express'
+import { Router } from 'express'
 
-import DronesRepository from '../repositories/DronesRepository'
-import DroneService from '../services/DroneService'
+import DronesController from '../controllers/DronesController'
 
 const dronesRouter = Router()
-const dronesRepository = new DronesRepository()
 
-dronesRouter.get('/', (request: Request, response: Response) => {
-  const droneService = new DroneService(dronesRepository)
+const dronesController = new DronesController()
 
-  const drones = droneService.getAll()
+dronesRouter.get('/', dronesController.list)
 
-  return response.json(drones)
-})
+dronesRouter.get('/:id', dronesController.detail)
 
-dronesRouter.get('/:id', (request: Request, response: Response) => {
-  const { id } = request.params
+dronesRouter.post('/', dronesController.create)
 
-  const droneService = new DroneService(dronesRepository)
+dronesRouter.put('/:id', dronesController.update)
 
-  const drone = droneService.getById(id)
-
-  return response.json(drone)
-})
-
-dronesRouter.post('/', (request: Request, response: Response) => {
-  try {
-    const {
-      customer_name,
-      customer_image,
-      customer_address,
-      battery,
-      max_speed,
-      average_speed,
-      status,
-      current_fly
-    } = request.body
-
-    const droneService = new DroneService(dronesRepository)
-
-    const drone = droneService.create({
-      customer_name,
-      customer_image,
-      customer_address,
-      battery,
-      max_speed,
-      average_speed,
-      status,
-      current_fly
-    })
-
-    return response.json(drone)
-  } catch (error) {
-    return response.status(400).json({ error: error.message })
-  }
-})
+dronesRouter.delete('/:id', dronesController.delete)
 
 export default dronesRouter
